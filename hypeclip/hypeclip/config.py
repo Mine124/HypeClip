52ed7e82a27bfba8f59a179575442d058d305620
Mine124 <87577747+Mine124@users.noreply.github.com>
2026-08-25 04:56:01 -0400

    Update config.py

--- hypeclip/hypeclip/config.py
+++ hypeclip/hypeclip/config.py
@@ -3,8 +3,7 @@ import os
 import sys
 from dataclasses import dataclass, fields
 
-APP_VERSION = "3.4.0"
-
+APP_VERSION = "3.5.0"
 
 def resource_dir() -> str:
     if getattr(sys, "frozen", False):
